964b4c20cf404d7f78f92b9a0500fb31ea7ea72a
Dean Pierrot <dapierrot21@gmail.com>
2024-01-22 22:45:50 -0500

    refactor: Organize DNA sequencing functions into a BioSequence class
    Create a BioSequence class to encapsulate DNA sequence-related functions. The class includes an initializer for sequence, label, and type, along with a method to validate the sequence and display sequence information. This refactoring enhances code organization and encapsulation for better maintainability.

--- bio_sequence.py
+++ bio_sequence.py
@@ -1,2 +1,26 @@
-class bio_sequence:
+from bio_structures import nucleotides
+from utilities import coloring_nucleotides
+
+
+class BioSequence:
     """DNA sequence class. Default value: ATCG, DNA No label"""
+
+    def __init__(self, dna_sequence="ATCG", sequence_type="DNA", label="No Label"):
+        """Sequence initialzation and sequence validation."""
+        self.dna_sequence = dna_sequence.upper()
+        self.label = label
+        self.sequence_type = sequence_type
+        self.is_valid = self.validate_sequence()
+        assert (
+            self.is_valid
+        ), f"Provided data does not seem to be a correct {self.sequence_type} sequence."
+
+    # Checkig the sequence to make sure it is a DNA string.
+
+    def validate_sequence(self):
+        """Checkig the sequence to make sure it is a DNA string."""
+        return set(nucleotides).issuperset(self.dna_sequence)
+
+    def show_sequence_info(self):
+        """Returns Sequence Information"""
+        return f"[Label]: {self.label}\n[Sequence]: {coloring_nucleotides(self.dna_sequence)}\n[Biotype]: {self.sequence_type}\n[Sequence Length]: {len(self.dna_sequence)}"
